50d847325c1bc02300dbb6f10cbe1d5b94066577
moges7624 <moges7624@gmail.com>
2025-04-06 09:18:27 +0300

    Add: parsing return statement

--- ast/ast.go
+++ ast/ast.go
@@ -48,3 +48,13 @@ func (p *Program) TokenLiteral() string {
     return ""
   }
 }
+
+type ReturnStatement struct {
+  Token token.Token
+  ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string {
+return rs.Token.Literal
+}
--- parser/parser.go
+++ parser/parser.go
@@ -57,6 +57,8 @@ func (p *Parser) parseStatement() ast.Statement {
   switch p.curToken.Type {
   case token.LET:
     return p.parseLetStatement()
+  case token.RETURN:
+    return p.parseReturnStatement()
   default: 
     return nil
   }
@@ -80,6 +82,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
     p.nextToken()
   }
 
+  return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement{
+  stmt := &ast.ReturnStatement{Token: p.curToken}
+
+  p.nextToken()
+
+  // TODO: We're skippiong the expression until ;
+  if !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+  }
   return stmt
 }
 
