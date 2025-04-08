5f676fe53faf0ee148f465fc448eecae2289845e
moges7624 <moges7624@gmail.com>
2025-04-08 22:06:34 +0300

    Add: parsing prefix operators

--- parser/parser.go
+++ parser/parser.go
@@ -50,6 +50,8 @@ func New(l *lexer.Lexer) *Parser {
   p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
   p.registerPrefix(token.IDENT, p.parseIdentifier)
   p.registerPrefix(token.INT, p.parseIntegerLiteral)
+  p.registerPrefix(token.BANG, p.parsePrefixExpression)
+  p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
   p.nextToken()
   p.nextToken()
@@ -170,6 +172,7 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
   prefix := p.prefixParseFns[p.curToken.Type]
 
   if prefix == nil {
+    p.noPefixParseFnError(p.curToken.Type)
     return nil
   }
 
@@ -190,4 +193,22 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 
   lit.Value = value
   return lit
+}
+
+func (p *Parser) noPefixParseFnError(t token.TokenType) {
+  msg := fmt.Sprintf("no prefix parse function for %s found", t)
+  p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+  expression := &ast.PrefixExpression{
+    Token: p.curToken,
+    Operator: p.curToken.Literal,
+  }
+
+  p.nextToken()
+
+  expression.Right = p.parseExpression(PREFIX)
+  
+  return expression
 }
