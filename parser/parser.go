cff777781b8d43c39498928a599227e28a2355aa
moges7624 <moges7624@gmail.com>
2025-04-13 22:44:19 +0300

    Add: parsing boolean

--- parser/parser.go
+++ parser/parser.go
@@ -73,6 +73,10 @@ func New(l *lexer.Lexer) *Parser {
   p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
   p.registerInfix(token.LT, p.parseInfixExpression)
   p.registerInfix(token.GT, p.parseInfixExpression)
+
+  p.registerPrefix(token.TRUE, p.parseBoolean)
+  p.registerPrefix(token.FALSE, p.parseBoolean)
+
   p.nextToken()
   p.nextToken()
 
@@ -272,4 +276,8 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
   expression.Right = p.parseExpression(precedence)
 
   return expression
+}
+
+func (p *Parser) parseBoolean() ast.Expression {
+  return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
 }
