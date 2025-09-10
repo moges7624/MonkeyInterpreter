6da75d4c62a7c1fe694d300271daaf130817b61a
moges7624 <moges7624@gmail.com>
2025-09-10 21:22:42 +0300

    fix: update parseLetStatement to get proper value

--- parser/parser.go
+++ parser/parser.go
@@ -148,7 +148,11 @@ func (p *Parser) parseLetStatement() ast.Statement {
     return nil
   }
 
-  for !p.curTokenIs(token.SEMICOLON) {
+  p.nextToken()
+
+  stmt.Value = p.parseExpression(LOWEST)
+
+  if p.peekTokenIs(token.SEMICOLON) {
     p.nextToken()
   }
 
