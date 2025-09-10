07abf06d5541e59a71426caccbb8b6fe40c02b26
moges7624 <moges7624@gmail.com>
2025-09-10 21:24:10 +0300

    fix: parseReturnStmt to get proper return value

--- parser/parser.go
+++ parser/parser.go
@@ -164,7 +164,9 @@ func (p *Parser) parseReturnStatement() ast.Statement {
 
   p.nextToken()
 
-  for  !p.curTokenIs(token.SEMICOLON) {
+  stmt.ReturnValue = p.parseExpression(LOWEST)
+
+  if p.peekTokenIs(token.SEMICOLON) {
     p.nextToken()
   }
 
