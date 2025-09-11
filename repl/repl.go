816d603851126266f1ba6c19f17e38082941cd98
moges7624 <moges7624@gmail.com>
2025-09-12 00:21:39 +0300

    feat: add repl to print ast of input program

--- repl/repl.go
+++ repl/repl.go
@@ -7,10 +7,24 @@ import (
 
 	"github.com/moges7624/MonkeyInterpreter/lexer"
 	"github.com/moges7624/MonkeyInterpreter/token"
+	"github.com/moges7624/MonkeyInterpreter/parser"
 )
 
 const PROMPT = ">> "
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
+// returns the tokens of the input program
 func Start(in io.Reader, out io.Writer) {
   scanner := bufio.NewScanner(in)
 
@@ -27,5 +41,41 @@ func Start(in io.Reader, out io.Writer) {
     for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
       fmt.Fprintf(out, "%+v\n", tok)
     }
+  }
+}
+
+// returns the ast of the input program
+func StartParse(in io.Reader, out io.Writer) {
+  io.WriteString(out, MONKEY_FACE)
+  scanner := bufio.NewScanner(in)
+
+  for  {
+    fmt.Fprintf(out, PROMPT)
+    scanned := scanner.Scan()
+    if !scanned {
+      return
+    }
+
+    line := scanner.Text()
+    l := lexer.New(line)
+    p := parser.New(l)
+
+    program := p.ParseProgram()
+    if len(p.Errors()) != 0 {
+      printParserErrors(out, p.Errors())
+      continue
+    }
+
+    io.WriteString(out, program.String())
+    io.WriteString(out, "\n")
+  }
+}
+
+
+func printParserErrors(out io.Writer, errors []string) {
+  io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+  io.WriteString(out, " parser errors:\n")
+  for _, msg := range errors {
+    io.WriteString(out, "\t"+msg+"\n")
   }
 }
