9f265f28860193b661ca5a3a50de071022b3a4bd
moges7624 <moges7624@gmail.com>
2025-09-12 13:00:35 +0300

    feat: evaluate integer literal

---
+++ evaluator/evaluator.go
@@ -0,0 +1,34 @@
+package evaluator
+
+import (
+	"github.com/moges7624/MonkeyInterpreter/ast"
+	"github.com/moges7624/MonkeyInterpreter/object"
+)
+
+func evalStatements(stmts []ast.Statement) object.Object {
+  var result object.Object
+
+  for _, statement := range stmts {
+    result = Eval(statement)
+  }
+
+  return result
+}
+
+func Eval(node ast.Node) object.Object {
+  switch node := node.(type) {
+
+  // statements
+  case *ast.Program:
+    return evalStatements(node.Statements)
+
+  case *ast.ExpressionStatement:
+    return Eval(node.Expression)
+
+  case *ast.IntegerLiteral:
+    return &object.Integer{Value: node.Value}
+  }
+
+  return nil
+}
+
