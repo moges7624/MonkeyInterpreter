652e8c149e4f96a172a2fe7e5b92732af0e6b9ef
moges7624 <moges7624@gmail.com>
2025-09-12 20:36:06 +0300

    feat: evaluate prefix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -8,6 +8,7 @@ import (
 var (
   TRUE = &object.Boolean{Value: true}
   FALSE = &object.Boolean{Value: false}
+  NULL = &object.Null{}
 )
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
@@ -27,6 +28,39 @@ func evalStatements(stmts []ast.Statement) object.Object {
   return result
 }
 
+func evalBangOperatorExpression(right object.Object) object.Object {
+  switch right {
+  case TRUE:
+    return FALSE
+  case FALSE:
+    return TRUE
+  case NULL:
+    return TRUE
+  default:
+    return FALSE
+  }
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+  if right.Type() != object.INTEGER_OBJ {
+    return NULL
+  }
+  
+  value := right.(*object.Integer).Value
+  return &object.Integer{Value: -value}
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+  switch operator {
+  case "!":
+    return evalBangOperatorExpression(right)
+  case "-":
+    return evalMinusPrefixOperatorExpression(right)
+  default:
+    return NULL
+  }
+}
+
 func Eval(node ast.Node) object.Object {
   switch node := node.(type) {
 
@@ -36,6 +70,10 @@ func Eval(node ast.Node) object.Object {
 
   case *ast.ExpressionStatement:
     return Eval(node.Expression)
+
+  case *ast.PrefixExpression:
+    right := Eval(node.Right)
+    return evalPrefixExpression(node.Operator, right)
 
   case *ast.IntegerLiteral:
     return &object.Integer{Value: node.Value}
