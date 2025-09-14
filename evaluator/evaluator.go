f0d791486af2298d2543a614080efb30ca302465
moges7624 <moges7624@gmail.com>
2025-09-14 14:37:42 +0300

    feat: concatenate strings using + operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -123,6 +123,20 @@ func evalIntegerInfixExpression(
   }
 }
 
+func evalStringInfixExpression(
+  operator string,
+  left, right object.Object,
+) object.Object {
+  if operator != "+" {
+    return newError("unknown operator: %s %s %s", 
+      left.Type(), operator, right.Type())
+  }
+    
+  leftVal := left.(*object.String).Value
+  rightVal := right.(*object.String).Value
+  return &object.String{Value: leftVal + rightVal}
+}
+
 func evalInfixExpression(
   operator string, 
   left, right object.Object,
@@ -134,6 +148,9 @@ func evalInfixExpression(
   case left.Type() == object.INTEGER_OBJ && 
     right.Type() == object.INTEGER_OBJ:
     return evalIntegerInfixExpression(operator, left, right)
+  case left.Type() == object.STRING_OBJ &&
+    right.Type() == object.STRING_OBJ:
+    return evalStringInfixExpression(operator, left, right)
   case operator == "==":
     return nativeBoolToBooleanObject(left == right)
   case operator == "!=":
