8d568bbd11d6a7a97aafb605037723e96c6f155a
moges7624 <moges7624@gmail.com>
2025-09-14 23:08:27 +0300

    feat: builtin puts function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/moges7624/MonkeyInterpreter/object"
+import (
+	"fmt"
+
+	"github.com/moges7624/MonkeyInterpreter/object"
+)
 
 var builtins = map[string]*object.Builtin {
   "len": &object.Builtin{
@@ -107,6 +111,15 @@ var builtins = map[string]*object.Builtin {
       newElements[length] = args[1]
 
       return &object.Array{Elements: newElements}
+    },
+  },
+  "puts" : {
+    Fn: func(args ...object.Object) object.Object {
+      for _, arg := range args {
+        fmt.Println(arg.Inspect())
+      }
+
+      return NULL
     },
   },
 }
